9066bdae6ebe4af674007516f98bbf80c641ae66
sabinbogati1 <sabin.bogati@studentpartner.com>
2018-01-14 11:37:55 +0545

    Finished Auth Flow

---
+++ config/keys.js
@@ -0,0 +1,13 @@
+
+if(process.env.NODE_ENV ==="production"){
+
+  module.exports=  require("./prod");
+
+}
+
+else{
+
+    module.exports = require("./dev");
+
+}
+
---
+++ services/passport.js
@@ -0,0 +1,51 @@
+var passport = require("passport");
+var mongoose = require("mongoose");
+var GoogleStrategy=require("passport-google-oauth20").Strategy;
+
+const keys = require("./../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user,done) =>{
+    done(null,user.id);
+
+});
+
+
+ passport.deserializeUser((id,done) =>{
+        User.findById(id)
+        .then(user => {
+            done(null,user);
+        })
+ });
+
+
+
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL : "/auth/google/callback"
+}, (accessToken,refreshToken,profile,done)=>{
+        
+  //  
+
+  User.findOne({ googleId: profile.id }) 
+  .then( (existingUser)=>{
+            if(existingUser){
+
+                console.log("the given user is already present... ");
+                done(null,existingUser);
+
+             }
+            else{
+                console.log("Create New   User");
+                //don't have User then create new one
+                new User({ googleId: profile.id }).save()
+                .then(user => done(null,user) )
+            }
+
+  })
+        
+
+})
+);
