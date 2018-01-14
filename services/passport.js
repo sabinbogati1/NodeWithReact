c1fafa90c03a7e70e68c880c65bc5396dc1c6e6e
sabinbogati1 <sabin.bogati@studentpartner.com>
2018-01-14 11:54:41 +0545

    google Strategy

--- services/passport.js
+++ services/passport.js
@@ -24,7 +24,8 @@ passport.serializeUser((user,done) =>{
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL : "/auth/google/callback"
+    callbackURL : "/auth/google/callback",
+    proxy:true
 }, (accessToken,refreshToken,profile,done)=>{
         
   //  
